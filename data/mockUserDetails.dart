aad51f979486889bd992655a72297b0e921ad7b6
apizbaha <63505878+apizbaha@users.noreply.github.com>
2020-05-09 17:02:51 +0800

    Delete mockUserDetails.dart

--- data/mockUserDetails.dart
+++
@@ -1,6 +0,0 @@
-
-import 'package:mobileapp/data/userDetails.dart';
-
-List<UserDetails> mockUserDetails = [
-  UserDetails("JohnDoe","Ahmad John Doe","0112345678","Kolej Tun Hussein Onn, UTM","johnDoe88@gmail.com")
-];
