86310640e9f7aa16543a3d2973d6a61dc11bc7fc
Faiz & Hazim <38588164+faizkarim98@users.noreply.github.com>
2020-05-09 16:14:04 +0800

    faiz

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,6 +5,10 @@ import 'listing.dart';
 import 'home.dart';
 import 'notificationscreen.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import './modal/mockItem.dart';
+import './modal/mockNotification.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import './data/mockUserDetails.dart';
 
 Color maincolor = Color(0xFFF1E90FF);
 Color dark = Color(0xff292b2c);
@@ -16,11 +20,11 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   var screen = [
-    HomeScreen(),
-    NotificationScreen(),
+    HomeScreen(mockItem),
+    ListingScreen(mockItem),
     SellScreen(),
-    ListingScreen(),
-    ProfileScreen(),
+    NotificationScreen(mockNotification),
+    ProfileScreen(mockUserDetails[0]),
   ];
 
   int screenIndex = 0;
@@ -52,94 +56,77 @@ class _HomepageState extends State<Homepage> {
         //     ],
         //   ),
         // ),
-        bottomNavigationBar: BottomNavigationBar(
-            currentIndex: screenIndex,
-            onTap: (index) {
+        bottomNavigationBar: BottomNavyBar(
+            itemCornerRadius: 100.0,
+            selectedIndex: screenIndex,
+            onItemSelected: (index) {
               print(index);
               setState(() {
                 screenIndex = index;
               });
             },
-            unselectedIconTheme: IconThemeData(
-              color: Colors.grey,
-            ),
-            selectedIconTheme: IconThemeData(
-              color: dark,
-            ),
-            selectedItemColor: dark,
-            selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-            type: BottomNavigationBarType.fixed,
             items: [
-              BottomNavigationBarItem(
-                  activeIcon: Icon(EvaIcons.home),
-                  icon: Icon(
-                    EvaIcons.homeOutline,
-                  ),
-                  title: Text(
-                    'Home',
-                    style: TextStyle(fontSize: 12.0),
-                  )),
-              BottomNavigationBarItem(
-                activeIcon: Icon(EvaIcons.bell),
-                icon: new Stack(
-                  children: <Widget>[
-                    new Icon(
-                      EvaIcons.bellOutline,
-                    ),
-                    new Positioned(
-                        right: 0,
-                        child: new Container(
-                          padding: EdgeInsets.all(2),
-                          decoration: new BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          constraints: BoxConstraints(
-                            minWidth: 12,
-                            minHeight: 12,
-                          ),
-                          child: new Text(
-                            '2',
-                            style:
-                                new TextStyle(color: Colors.white, fontSize: 8),
-                            textAlign: TextAlign.center,
-                          ),
-                        )),
-                  ],
-                ),
-                title: Text('Notifications', style: TextStyle(fontSize: 12.0)),
+              BottomNavyBarItem(
+                title: Text('Home'),
+                inactiveColor: Colors.grey,
+                activeColor: Colors.blueAccent,
+                icon: Icon(EvaIcons.homeOutline),
+                textAlign: TextAlign.center,
               ),
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    EvaIcons.plusSquare,
-                    size: 30,
-                    color: Colors.blueAccent,
-                  ),
-                  title: Text(
-                    'Sell',
-                    style: TextStyle(fontSize: 12.0),
-                  )),
-              BottomNavigationBarItem(
-                  activeIcon: Icon(
-                    EvaIcons.grid,
-                  ),
-                  icon: Icon(
-                    EvaIcons.gridOutline,
-                    size: 22,
-                  ),
-                  title: Text(
-                    'My Listing',
-                    style: TextStyle(fontSize: 12.0),
-                  )),
-              BottomNavigationBarItem(
-                  activeIcon: Icon(EvaIcons.person),
-                  icon: Icon(
-                    EvaIcons.personOutline,
-                  ),
-                  title: Text(
-                    'Me',
-                    style: TextStyle(fontSize: 12.0),
-                  )),
+              BottomNavyBarItem(
+                title: Text('My Listings'),
+                inactiveColor: Colors.grey,
+                textAlign: TextAlign.center,
+                activeColor: Colors.blueAccent,
+                icon: Icon(EvaIcons.gridOutline),
+              ),
+              //  BottomNavItem(
+              //     activeIcon: Icon(EvaIcons.bell),
+              //     icon: new Stack(
+              //       children: <Widget>[
+              //         new Icon(
+              //           EvaIcons.bellOutline,
+              //         ),
+              //         new Positioned(
+              //             right: 0,
+              //             child: new Container(
+              //               padding: EdgeInsets.all(2),
+              //               decoration: new BoxDecoration(
+              //                 color: Colors.red,
+              //                 borderRadius: BorderRadius.circular(6),
+              //               ),
+              //               constraints: BoxConstraints(
+              //                 minWidth: 12,
+              //                 minHeight: 12,
+              //               ),
+              //               child: new Text(
+              //                 '2',
+              //                 style:
+              //                     new TextStyle(color: Colors.white, fontSize: 8),
+              //                 textAlign: TextAlign.center,
+              //               ),
+              //             )),
+              //       ],
+              //     ),
+              //     title: Text('Notifications', style: TextStyle(fontSize: 12.0)),
+              //   ),
+              BottomNavyBarItem(
+                  title: Text('Sell'),
+                  activeColor: Colors.blueAccent,
+                  icon: Icon(EvaIcons.plusOutline),
+                  textAlign: TextAlign.center),
+              BottomNavyBarItem(
+                  title: Text('Notifications'),
+                  inactiveColor: Colors.grey,
+                  activeColor: Colors.blueAccent,
+                  icon: Icon(EvaIcons.bellOutline),
+                  textAlign: TextAlign.center),
+              BottomNavyBarItem(
+                  title: Text('Me'),
+                  inactiveColor: Colors.grey,
+                  activeColor: Colors.blueAccent,
+                  icon: Icon(EvaIcons.personOutline),
+                  textAlign: TextAlign.center),
             ]),
         body: screen[screenIndex]);
   }
