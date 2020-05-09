43ef6c1f887ed4e714f7fff7283961013d611317
apizbaha <63505878+apizbaha@users.noreply.github.com>
2020-05-09 16:54:09 +0800

    Hafiz Baharudin

---
+++ data/mockUserDetails.dart
@@ -0,0 +1,6 @@
+
+import 'package:mobileapp/data/userDetails.dart';
+
+List<UserDetails> mockUserDetails = [
+  UserDetails("JohnDoe","Ahmad John Doe","0112345678","Kolej Tun Hussein Onn, UTM","johnDoe88@gmail.com")
+];
