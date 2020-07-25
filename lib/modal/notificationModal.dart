7fb834c1a898f66376b9da71589109d26f0ec34e
Faiz Karim <38588164+faizkarim98@users.noreply.github.com>
2020-07-25 19:27:58 +0800

    Faiz Karim

--- lib/modal/notificationModal.dart
+++ lib/modal/notificationModal.dart
@@ -1,11 +1,30 @@
-class Notifications{
+class Notifications {
   String text1;
   String text2;
   String date;
   String time;
+  String id;
   bool read;
 
-  Notifications(this.text1,this.text2,this.date,this.time,this.read);
+  Notifications(
+      {this.id, this.text1, this.text2, this.date, this.time, this.read});
+  factory Notifications.fromJson(Map<String, dynamic> json) {
+    return Notifications(
+      id: json['id'],
+      text1: json['text1'],
+      text2: json['text2'],
+      date: json['date'],
+      time: json['time'],
+      read: json['read'],
+    );
+  }
 
-  Notifications.copy(Notifications from) : this(from.text1,from.text2,from.date,from.time,from.read);
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'text1': text1,
+        'text2': text2,
+        'date': date,
+        'time': time,
+        'read': read,
+      };
 }
