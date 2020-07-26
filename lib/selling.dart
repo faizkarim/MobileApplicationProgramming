4b0908b24a38d1358f1adb61aa98053295d447e7
Faiz Karim <38588164+faizkarim98@users.noreply.github.com>
2020-07-26 14:17:02 +0800

    Faiz Karim

--- lib/selling.dart
+++ lib/selling.dart
@@ -1,14 +1,12 @@
-import 'dart:io';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:mobileapp/modal/mockItem.dart';
-import 'package:mobileapp/modal/mockNotification.dart';
-import './modal/itemModal.dart';
 import 'package:snack/snack.dart';
 import 'dart:async';
-import './modal/notificationModal.dart';
 import 'package:intl/intl.dart';
+import './modal/notificationModal.dart';
+import './services/notification_services.dart';
+import './modal/itemModal.dart';
+import './services/item_services.dart';
 
 class SellScreen extends StatefulWidget {
   @override
@@ -28,7 +26,7 @@ class _SellScreenState extends State<SellScreen> {
         ],
       ));
 
-  File imageFile;
+  static String imageUrl;
   static String listingName;
   static String price;
   static String description;
@@ -54,26 +52,30 @@ class _SellScreenState extends State<SellScreen> {
       }
 
       Item itemdata = new Item(
-          imageFile,
-          listingName,
-          price,
-          ItemDetails(_category, description, _location),
-          Seller('johndoe817', '018-4563423', 'john@gmail.com'));
+          itemImg: imageUrl,
+          itemName: listingName,
+          price: price,
+          save: false,
+          itemdetails: ItemDetails(
+              category: _category, description: description, place: _location));
+
+      ItemService().addItem(n: itemdata);
+
       Notifications noti = new Notifications(
-          'Your item succesfully in Listing',
-          'You now can check your item in My Listing to edit the item',
-          dateTime[0],
-          dateTime[1],
-          false);
-      mockNotification.add(noti);
-      mockItem.add(itemdata);
+          text1: 'Your item succesfully in Listing',
+          text2: 'You now can check your item in Home to view the item',
+          date: dateTime[0],
+          time: dateTime[1],
+          read: false);
+
+      NotificationService().addNotifications(n: noti);
 
       bar.show(context);
       await Future.delayed(Duration(seconds: 2));
       Navigator.pushNamed(context, '/homescreen');
     } else {
       setState(() {
-        print(imageFile);
+        print(imageUrl);
         _autoValidate = true;
       });
     }
@@ -85,13 +87,6 @@ class _SellScreenState extends State<SellScreen> {
     else
       return null;
   }
-
-  // String validatePrice(double value) {
-  //   if (value.isNegative)
-  //     return "Price cannot be negative";
-  //   else
-  //     return null;
-  // }
 
   String validateDescription(String value) {
     if (value.isEmpty)
@@ -133,52 +128,6 @@ class _SellScreenState extends State<SellScreen> {
                   ),
                 ),
                 SizedBox(height: 20.0),
-                GestureDetector(
-                  onTap: () async {
-                    var picture = await ImagePicker.pickImage(
-                        source: ImageSource.gallery);
-                    // Directory picDir = await getApplicationDocumentsDirectory();
-                    // path = picDir.uri.resolve('assets/images/picture.jpg').path;
-                    // file = await picture.copy(path);
-                    setState(() {
-                      imageFile = picture;
-                    });
-                  },
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 200.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5.0),
-                      border: Border.all(width: 1.0, color: Colors.black26),
-                      // image: DecorationImage(
-                      //     image: new FileImage(File(widget.imagePath)),
-                      //     fit: BoxFit.fill),
-                    ),
-                    child: imageFile != null
-                        ? Image.file(
-                            imageFile,
-                            fit: BoxFit.fill,
-                          )
-                        : Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(
-                                Icons.add_a_photo,
-                                color: Colors.black45,
-                              ),
-                              SizedBox(
-                                height: 2.0,
-                              ),
-                              Text(
-                                "Tap to add photo",
-                                style: TextStyle(
-                                    color: Colors.black45,
-                                    fontFamily: 'OpenSans'),
-                              ),
-                            ],
-                          ),
-                  ),
-                ),
                 SizedBox(height: 20.0),
                 Form(
                   autovalidate: _autoValidate,
@@ -186,6 +135,38 @@ class _SellScreenState extends State<SellScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
+                      Text(
+                        "Image",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'OpenSans'),
+                      ),
+                      SizedBox(height: 10.0),
+                      TextFormField(
+                        validator: validatelistingName,
+                        onSaved: (String val) {
+                          setState(() {
+                            imageUrl = val;
+                          });
+                        },
+                        decoration: InputDecoration(
+                            fillColor: Colors.grey[100],
+                            filled: true,
+                            // prefixIcon: Icon(Icons.search),
+                            enabledBorder: const OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.transparent, width: 0.0),
+                            ),
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                  color: Colors.transparent, width: 0.0),
+                            ),
+                            hintText: 'Your Image Url',
+                            hintStyle: TextStyle(
+                                fontSize: 15, fontFamily: 'OpenSans')),
+                      ),
+                      SizedBox(height: 20.0),
                       Text(
                         "Category",
                         style: TextStyle(
