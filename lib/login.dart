5e2de01a3116703bcf639f9742fdd1f025ae8e53
apizbaha <63505878+apizbaha@users.noreply.github.com>
2020-07-26 13:57:33 +0800

    add firebase auth to login

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './data/mockUserDetails.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 Color maincolor = Color(0xFFF1E90FF);
 Color secondaryColor = Color(0xFFF3881E3);
@@ -9,13 +10,39 @@ class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
 }
-
+  
 class _LoginState extends State<Login> {
-  TextEditingController username = new TextEditingController();
-  TextEditingController password = new TextEditingController();
+  final formKey = new GlobalKey<FormState>();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  String _email;
+  String _password;
+  String errorMessage;
+ 
+  bool saveForm() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    } else {
+      return false;
 
-  String errormessage;
+    }
+  }
 
+  void submitForm() async{
+    if (saveForm()) {
+      try {
+        FirebaseUser user = (await _auth.signInWithEmailAndPassword(email: _email, password: _password)).user;
+        print('Signed in: ${user.uid}');
+        Navigator.pushReplacementNamed(context, '/loaderspinnerscreen');
+      }
+      catch (e) {
+        setState(() {
+          errorMessage = "Invalid email or password";
+        });
+      }
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,116 +55,112 @@ class _LoginState extends State<Login> {
           child: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width - 60,
-              child: Column(
-                children: <Widget>[
-                  Image(image: AssetImage('assets/images/logo.png')),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 50.0,
-                      ),
-                      Text(
-                        'Login',
-                        style: TextStyle(
-                            color: maincolor,
-                            fontFamily: 'OpenSans',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25.0),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 20.0,
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width - 60,
-                            child: TextField(
-                              controller: username,
-                              decoration: InputDecoration(
-                                  errorText: errormessage,
-                                  labelText: 'Username',
-                                  labelStyle:
-                                      TextStyle(fontFamily: 'OpenSans')),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20.0,
-                          ),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width - 60,
-                            child: TextField(
-                              controller: password,
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                  errorText: errormessage,
-                                  labelText: 'Password',
-                                  labelStyle:
-                                      TextStyle(fontFamily: 'OpenSans')),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25.0,
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5.0),
-                        color: Colors.blueAccent),
-                    child: FlatButton(
-                      onPressed: () {
-                        if (username.text == mockUserDetails[0].userName &&
-                            password.text == mockUserDetails[0].password) {
-                          print(username.text);
-                          Navigator.pushReplacementNamed(
-                              context, '/loaderspinnerscreen');
-                        }
-                        if (username.text.isEmpty) {
-                          setState(() {
-                            errormessage =
-                                'Username or password cannot be empty';
-                          });
-                        } else {
-                          setState(() {
-                            errormessage = 'Invalid username or password';
-                          });
-                        }
-                      },
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'OpenSans',
-                            fontWeight: FontWeight.bold),
-                      ),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width - 60,
+                child: Column(
+                  children: <Widget>[
+                    Image(image: AssetImage('assets/images/logo.png')),
+                    SizedBox(
+                      height: 20.0,
                     ),
-                  )
-                ],
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        SizedBox(
+                          height: 50.0,
+                        ),
+                        Text(
+                          'Login',
+                          style: TextStyle(
+                              color: maincolor,
+                              fontFamily: 'OpenSans',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25.0),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width - 60,
+                              child: TextFormField(
+                                key: new Key('email'),
+                                validator: (value) => value.isEmpty ? 'Email cannot be empty' : null,
+                                onSaved: (value) => _email = value.trim(),
+                                autocorrect: false,
+                                decoration: InputDecoration(
+                                    errorText: errorMessage,
+                                    labelText: 'Email',
+                                    labelStyle:
+                                        TextStyle(fontFamily: 'OpenSans')),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width - 60,
+                              child: TextFormField(
+                                key: new Key('password'),
+                                validator: (value) => value.isEmpty ? 'Password cannot be empty' : null,
+                                onSaved: (value) => _password = value,
+                                obscureText: true,
+                                autocorrect: false,
+                                
+                                decoration: InputDecoration(
+                                    errorText: errorMessage,
+                                    labelText: 'Password',
+                                    labelStyle:
+                                        TextStyle(fontFamily: 'OpenSans')),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: Colors.blueAccent),
+                      child: FlatButton(
+                        onPressed: submitForm,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'OpenSans',
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      
+                      
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       )),
     );
   }
 }
+
