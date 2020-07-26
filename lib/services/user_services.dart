0f155538d9d421f168444ce46c77513d32673f80
apizbaha <63505878+apizbaha@users.noreply.github.com>
2020-07-26 14:07:51 +0800

    Update user_services.dart

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -10,19 +10,27 @@ class UserService {
   UserService._constructor();
   final rest = RestService();
 
-  Future<User> getUser({String id}) async {
-    final json = await rest.patch('users/$id');
+  Future<List<User>> getAllUsers() async {
+    final listJson = await rest.get('users');
 
+    return (listJson as List)
+        .map((itemJson) => User.fromJson(itemJson))
+        .toList();
+  }
+
+  Future<User> getUser({String id}) async {
+    final json = await rest.get('users/$id');
     return User.fromJson(json);
   }
 
-  Future<User> addUser({User n}) async {
-    final json = await rest.post('users', data: n.toJson());
+  Future<User> addUser({User user}) async {
+    final json = await rest.post('users', data: user.toJson());
     return User.fromJson(json);
   }
 
   Future<User> updateUser({String id, User u}) async {
-    final json = await rest.patch('users/$id', data: u.toJson());
+    final json = await rest.patch('users/$id', data: u);
     return User.fromJson(json);
   }
 }
+
